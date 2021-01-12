3c40d3d994b543ba412bd37f7a0f42113f01941e
HerbivorousApe <60274573+HerbivorousApe@users.noreply.github.com>
2021-01-12 14:55:18 -0600

    MenuController.cs was causing issues with WeatherThisWeb - removed it an added control to the view

--- WeatherThisConsole/Views/MenuView.cs
+++ WeatherThisConsole/Views/MenuView.cs
@@ -9,8 +9,6 @@ namespace WeatherThisConsole.Views
 
         public async Task Menu()
         {
-            var menuController = new MenuController();
-
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("  1. Seven Day Forecast");
             Console.WriteLine("  2. Seven Day Forecast (Hourly)");
@@ -23,8 +21,38 @@ namespace WeatherThisConsole.Views
 
             ConsoleKey menuChoice = Console.ReadKey(true).Key;
 
-            await menuController.Menu(menuChoice);
-        }
+            var view = new MainWelcomeView();
+            var misc = new MiscController();
+            var apiView = new APICallsView();
+            var sevenDayForecast = new SevenDayForecastView();
+            var sevenDayForecastHourly = new SevenDayForecastHourlyView();
+            var sevenDayHistoryHourly = new SevenDayHistoryHourlyView();
+
+            switch (menuChoice)
+                {
+                    case ConsoleKey.D1: await sevenDayForecast.SevenDayForecast(); break;
+                    case ConsoleKey.NumPad1: await sevenDayForecast.SevenDayForecast(); break;
+
+                    case ConsoleKey.D2: await sevenDayForecastHourly.SevenDayForecastHourly(); break;
+                    case ConsoleKey.NumPad2: await sevenDayForecastHourly.SevenDayForecastHourly(); break;
+
+
+                    case ConsoleKey.D3: await sevenDayHistoryHourly.SevenDayHistoryHourly(); break;
+                    case ConsoleKey.NumPad3: await sevenDayHistoryHourly.SevenDayHistoryHourly(); break;
+
+                    case ConsoleKey.D4: await apiView.UpdateZipView(); break;
+                    case ConsoleKey.NumPad4: await apiView.UpdateZipView(); break;
+
+                    
+                    case ConsoleKey.D5: misc.FlipIsImperial(); await view.Welcome(); break;
+                    case ConsoleKey.NumPad5: misc.FlipIsImperial(); await view.Welcome(); break;
+
+                    case ConsoleKey.Escape: Environment.Exit(0); break;
+
+                    default: await view.Welcome(); break;
+                }
+            }
+
         public async Task ReturnToWelcome()
         {
             var view = new MainWelcomeView();
